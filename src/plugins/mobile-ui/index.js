129eddf2026dcdf5c8f6217343306be712d60e8f
Charles Cross <charles@openrov.com>
2016-02-16 17:42:54 +0000

    Initial go at the mobile-ui theme

---
+++ src/plugins/mobile-ui/index.js
@@ -0,0 +1,15 @@
+function mobileUi( name, deps ) 
+{
+	console.log( 'Mobile UI plugin loaded.' );
+	
+	this.plugin =
+	{
+		name: "mobile-ui",
+		type: "theme"
+	};
+}
+
+module.exports = function( name,deps )
+{
+  return new mobileUi( name, deps );
+};
